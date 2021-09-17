0c25970866a574aea36eecbe0547ccd988a4ce8d
B-E-C-H <mikkel.l.bech@gmail.com>
2021-09-17 13:50:56 +0200

    Added Student stuff

--- Assignment02/Assignment02.ClassLibrary/Student.cs
+++ Assignment02/Assignment02.ClassLibrary/Student.cs
@@ -12,16 +12,42 @@ namespace Assignment02.ClassLibrary {
             Graduated
         }
 
-        public int Id { get; }
+        public int Id { get; init; }
         public string GivenName { get; set; }
         public string Surname { get; set; }
-        public StatusEnum Status { get; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public DateTime GraduationDate { get; set; }
+        public StatusEnum Status {
+            get {
+                if (DateTime.Now < StartDate) {
+                    // Hvis den nuværende dato er før Start Datoen
+                    return StatusEnum.New;
 
-        public Student() {
+                } else if (DateTime.Now > StartDate && DateTime.Now < EndDate) {
+                    // Hvis den nuværende tidspunkt er efter start dato men før End date. 
+                    return StatusEnum.Active;
+                } else if (DateTime.Now > GraduationDate) {
+                    // Hvis det nuværende tidspunkt er efter Graduation date.
+                    return StatusEnum.Graduated;
+                } else {
+                    // Hvis end date er før graduation date.
+                    return StatusEnum.Dropout;
+                }
+            }
+        }
+
+        public Student(string gn, string sn, DateTime sd, DateTime ed, DateTime gd) {
+            GivenName = gn;
+            Surname = sn;
+            StartDate = sd;
+            EndDate = ed;
+            GraduationDate = gd;
             Console.WriteLine("New student!");
+        }
+
+        public string toString() {
+            return $"{GivenName} {Surname} ({Status}) | {StartDate} - {EndDate} [{GraduationDate}]";
         }
     }
 }
--- Assignment02/Assignment02.Tests/UnitTest1.cs
+++ Assignment02/Assignment02.Tests/UnitTest1.cs
@@ -4,7 +4,12 @@ using Xunit;
 namespace Assignment02.Tests {
     public class UnitTest1 {
         [Fact]
-        public void Test1() {
+        public void testNewStudent() {
+
+            var student = new Assignment02.ClassLibrary.Student("Mikkel", "Bech", new DateTime(2010, 10, 10), new DateTime(2012, 10, 10), new DateTime(2013, 10, 10)) { Id = 10 };
+
+
+            Console.WriteLine(student);
 
         }
     }
